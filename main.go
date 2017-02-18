26ecfb43dc401fb94a4830ead0b9069f9bdc599f
j <mukjim@gmail.com>
2017-02-18 22:46:07 +0900

    fib ex

--- main.go
+++ main.go
@@ -2,13 +2,43 @@ package main
 
 import "fmt"
 
-func fib(n uint64) uint64 {
+func fib(n int) int {
+	var ret int
 	if n <= 0 {
-		return 1
+		// error val
+		ret = -1
+	} else if n == 1 {
+		ret = 0
+	} else if n == 2 || n == 3 {
+		ret = 1
+	} else {
+		ret = fib(n-1) + fib(n-2)
 	}
-	return fib(n-1) + fib(n-2)
+	return ret
+}
+
+func fib_(n int) int {
+	var p, q, ret int
+	p, q = 0, 1
+	for i := 3; i <= n; i++ {
+		p, q = q, p+q
+	}
+
+	if n <= 0 {
+		// error val
+		ret = -1
+	} else if n == 1 {
+		ret = 0
+	} else if n == 2 || n == 3 {
+		ret = 1
+	} else {
+		ret = q
+	}
+	return ret
 }
 
 func main() {
-	fmt.Println(fib(10))
+	for i := 1; i <= 10; i++ {
+		fmt.Println(fib_(i))
+	}
 }
